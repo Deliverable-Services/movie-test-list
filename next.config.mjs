404d925f4c48bf0ae969317d5b990c664e43fa44
Dishant Agnihotri <dshntagnihotri@gmail.com>
2024-11-21 17:15:26 +0530

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "/public/uploads/:path*", // Optional, if you need custom rewrites
+      },
+    ];
+  },
 };
 
 export default nextConfig;
